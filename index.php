e58fca3100fad24a76955f9686ca4db8c2071c7b
Kenyu Sakuraba <kenyu.sakuraba.1023@gmail.com>
2017-03-16 16:58:04 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,101 @@
+<?php include "partials/header.php"; ?>
+        
+<div class="page-content">
+    <div class="row">
+        <main class="main-content small-12 large-9 large-push-3 column">
+            
+            <article class="blog-post">
+                <header class="post-header">
+                    <h2 class="post-heading">article heading</h2>
+                    <p class="post-pub-date">
+                        <span class="pub-date-month">january</span> 
+                        <span class="pub-date-day">24</span>, 
+                        <span class="pub-date-year">2017</span></p>
+                </header>
+                <div class="post-excerpt">
+                    <p>asdf akldjf adkljf </p>
+                    <h3>subheading</h3>
+                </div>
+                <footer class="post-footer">
+                    <div class="post-author">
+                        <p class="author-name">Author Name</p>
+                        <p class="author-info">Author Description here</p>
+                        <a href="#" class="author-link">author link</a>
+                    </div>
+                </footer>
+            </article>
+        
+            <article class="blog-post">
+                <header class="post-header">
+                    <h2 class="post-heading">article heading</h2>
+                    <p class="post-pub-date">
+                        <span class="pub-date-month">january</span> 
+                        <span class="pub-date-day">24</span>, 
+                        <span class="pub-date-year">2017</span></p>
+                </header>
+                <div class="post-excerpt">
+                    <p>asdf akldjf adkljf </p>
+                    <h3>subheading</h3>
+                </div>
+                <footer class="post-footer">
+                    <div class="post-author">
+                        <p class="author-name">Author Name</p>
+                        <p class="author-info">Author Description here</p>
+                        <a href="#" class="author-link">author link</a>
+                    </div>
+                </footer>
+            </article>
+        
+            <article class="blog-post">
+                <header class="post-header">
+                    <h2 class="post-heading">article heading</h2>
+                    <p class="post-pub-date">
+                        <span class="pub-date-month">january</span> 
+                        <span class="pub-date-day">24</span>, 
+                        <span class="pub-date-year">2017</span></p>
+                </header>
+                <div class="post-excerpt">
+                    <p>asdf akldjf adkljf </p>
+                    <h3>subheading</h3>
+                </div>
+                <footer class="post-footer">
+                    <div class="post-author">
+                        <p class="author-name">Author Name</p>
+                        <p class="author-info">Author Description here</p>
+                        <a href="#" class="author-link">author link</a>
+                    </div>
+                </footer>
+            </article>
+        
+            <article class="blog-post">
+                <header class="post-header">
+                    <h2 class="post-heading">article heading</h2>
+                    <p class="post-pub-date">
+                        <span class="pub-date-month">january</span> 
+                        <span class="pub-date-day">24</span>, 
+                        <span class="pub-date-year">2017</span></p>
+                </header>
+                <div class="post-excerpt">
+                    <p>asdf akldjf adkljf </p>
+                    <h3>subheading</h3>
+                </div>
+                <footer class="post-footer">
+                    <div class="post-author">
+                        <p class="author-name">Author Name</p>
+                        <p class="author-info">Author Description here</p>
+                        <a href="#" class="author-link">author link</a>
+                    </div>
+                </footer>
+            </article>
+        
+        </main>
+        <div class="sidebar medium-12 large-3 large-pull-9 hide-for-small-only column">
+            <div class="widget">
+                <h3 class="widget-heading">widget heading</h3>
+                <p>here some content in a widget!</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include "partials/footer.php"; ?>
---
+++ partials/header.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Bloggety Blog</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        
+        <header class="page-header">
+            <div class="header-main">
+                <div class="row">
+                    <div class="site-logo medium-4 column">
+                        <img src="apple-touch-icon.png"
+                             alt="site logo">
+                    </div>
+                    <div class="site-title medium-8 column">
+                        <h1>Hobbitty Blog</h1>
+                        <p class="tagline">A Very Conspicuous Blog</p>
+                    </div>
+                </div>
+            </div>
+            <nav menu-main>
+                <div class="row">
+                    <ul class="small-12 column">
+                        <li><a href="#">item</a></li>
+                        <li><a href="#">item</a></li>
+                        <li><a href="#">item</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+
+        
+        
